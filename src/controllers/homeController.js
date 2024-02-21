596311dc53ec37b7a8030659685fc90a94101688
Tienzwuang <tienquang090901@gmail.com>
2024-02-22 01:41:59 +0700

    25

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,17 +1,30 @@
-import db from '../models/index';
+import db from "../models/index";
+import CRUDService from "../services/CRUDServices";
 
 let getHomePage = async (req, res) => {
   try {
     let data = await db.User.findAll();
 
-    return res.render('homepage.ejs',{
-      data: JSON.stringify(data)
+    return res.render("homepage.ejs", {
+      data: JSON.stringify(data),
     });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
+
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from server");
+};
 
 module.exports = {
-  getHomePage : getHomePage,
-}
+  getHomePage: getHomePage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
+};
